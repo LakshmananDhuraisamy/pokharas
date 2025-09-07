77f39274f2214bb6e17849426033a655bf5ec530
Lakshmanan D <dlakshmananbe@gmail.com>
2025-09-07 20:15:49 +0530

    htaccess files & url updates

--- header.php
+++ header.php
@@ -142,17 +142,17 @@ $property_banner = json_decode($term_result, true);
     <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
       rel="stylesheet">
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href="vendors/fontawesome-pro-5/css/all.css">
-    <link rel="stylesheet" href="vendors/bootstrap-select/css/bootstrap-select.min.css">
-    <link rel="stylesheet" href="vendors/slick/slick.min.css">
-    <link rel="stylesheet" href="vendors/magnific-popup/magnific-popup.min.css">
-    <link rel="stylesheet" href="vendors/jquery-ui/jquery-ui.min.css">
-    <link rel="stylesheet" href="vendors/chartjs/Chart.min.css">
-    <link rel="stylesheet" href="vendors/dropzone/css/dropzone.min.css">
-    <link rel="stylesheet" href="vendors/animate.css">
-    <link rel="stylesheet" href="vendors/timepicker/bootstrap-timepicker.min.css">
-    <link rel="stylesheet" href="vendors/mapbox-gl/mapbox-gl.min.css">
-    <link rel="stylesheet" href="vendors/dataTables/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="/vendors/fontawesome-pro-5/css/all.css">
+    <link rel="stylesheet" href="/vendors/bootstrap-select/css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="/vendors/slick/slick.min.css">
+    <link rel="stylesheet" href="/vendors/magnific-popup/magnific-popup.min.css">
+    <link rel="stylesheet" href="/vendors/jquery-ui/jquery-ui.min.css">
+    <link rel="stylesheet" href="/vendors/chartjs/Chart.min.css">
+    <link rel="stylesheet" href="/vendors/dropzone/css/dropzone.min.css">
+    <link rel="stylesheet" href="/vendors/animate.css">
+    <link rel="stylesheet" href="/vendors/timepicker/bootstrap-timepicker.min.css">
+    <link rel="stylesheet" href="/vendors/mapbox-gl/mapbox-gl.min.css">
+    <link rel="stylesheet" href="/vendors/dataTables/jquery.dataTables.min.css">
     <!-- Themes core CSS -->
     <link rel="stylesheet" href="css/themes.css">
     <!-- Favicons -->
